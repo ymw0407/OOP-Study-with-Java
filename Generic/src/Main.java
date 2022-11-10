6653c3b1e83eda0fd28229b8edb0e4fc8cf43f7c
Yun Min Woo <77202633+ymw0407@users.noreply.github.com>
2022-11-10 13:19:07 +0900

    Delete Generic directory

--- Generic/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Box<Integer> box = new Box<Integer>();
-        box.set(10);
-        
-        Integer i = box.get();
-        System.out.println(i.intValue());
-    }
-}
